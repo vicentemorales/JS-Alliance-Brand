1a23f7234159b96867a833f90ae35c273adb711c
vicentemorales <vmorales121@gmail.com>
2023-03-14 21:01:34 -0700

    modified the first 4 class examples

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -1,25 +1,35 @@
 var customerName = "Alliance  ";
 
 // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
+
+
 function addCustomerName() {
 
     let title = document.getElementById('title')
     title.textContent = customerName;
     
     // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
-
+       let header = document.getElementById('tm-header')
+       header.textContent = customerName; 
     // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
 
 };
 
 function toggleColors() {
     // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
-
+    let blue = document.getElementsByClassName('tm-white-rect')
+    blue[0].classList.add('tm-blue-rect')
+    blue[0].classList.remove('tm-white-rect')
+    
 };
+
+
+
+
 
 function hideElement() {
     //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
-
+    document.getElementsByClassName('nav flex-row tm-social-links').style.visibility='hidden';
 };
 
  function addText() {
